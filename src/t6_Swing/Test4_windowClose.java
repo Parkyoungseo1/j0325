43f9c8957b762c248b41c1f7bd5cd7a428bd932f
Breadtj <taking99@naver.com>
2024-03-25 18:13:34 +0900

    20240325-1813

---
+++ src/t6_Swing/Test4_windowClose.java
@@ -0,0 +1,30 @@
+package t6_Swing;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.JFrame;
+
+// 스윙의 종료를 어뎁터를 이용.
+@SuppressWarnings("serial")
+public class Test4_windowClose extends JFrame {
+	
+	
+	public Test4_windowClose() {
+		super("스윙연습4");
+		
+		setBounds(300, 250, 300, 250);
+		
+		setVisible(true);
+		
+		// 윈도우 창  닫기
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		new Test4_windowClose();
+	}
+
+
+}
